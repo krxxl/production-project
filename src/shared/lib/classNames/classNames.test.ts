6dfcd4b1883921d90cd194a1a7818642aa498190
krxxl <krxxl@mail.ru>
2023-02-09 11:53:19 +0400

    add jest and one test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,29 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('classNames with class', () => {
+    expect(classNames('class')).toBe('class');
+  });
+  test('classNames with additional class', () => {
+    expect(classNames('class', {}, ['class1', 'class2']))
+      .toBe('class class1 class2');
+  });
+  test('classNames with mods', () => {
+    const expected = 'class class1 class2 class3 class4';
+    expect(classNames(
+      'class',
+      { class3: true, class4: true },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
+  });
+  test('classNames with mods false and undefined', () => {
+    const expected = 'class class1 class2';
+    expect(classNames(
+      'class',
+      { class3: false, class4: undefined },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
+  });
+});
