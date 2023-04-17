937e53526f9bfaf363a5db1339252cb14923ae44
krxxl <krxxl@mail.ru>
2023-04-17 15:45:47 +0400

    refactor drawer add browserlist

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -44,11 +44,9 @@ export const NotificationButton = memo(({ className }: notificationButtonProps) 
       </BrowserView>
       <MobileView>
         {trigger}
-        <AnimationProvider>
-          <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-            <NotificationList />
-          </Drawer>
-        </AnimationProvider>
+        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+          <NotificationList />
+        </Drawer>
       </MobileView>
     </div>
   );
